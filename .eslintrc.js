1781ac11860ac6eeb33965ba004d50beffc5e41c
Grady Kuhnline <github@heygrady.net>
2017-03-01 15:43:28 -0800

    removing neutrino, copying build config from redux

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-const Neutrino = require('neutrino');
-// const pkg = require('./package.json');
-// const api = new Neutrino(pkg.config.presets);
-const api = new Neutrino(['neutrino-preset-standard', 'neutrino-preset-node']);
-
-module.exports = api.custom.eslintrc();
--- neutrino-custom.js
+++
@@ -1,8 +0,0 @@
-module.exports = neutrino => {
-  neutrino.config.module
-    .rule('compile')
-    .loader('babel', ({ options }) => {
-      options.presets[0][1].targets.node = 4.2
-      return { options }
-    })
-}
